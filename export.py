d449ae5c956a426d7e2e1d8781cb30d756d7329f
lorne <1991wangliang@gmail.com>
2025-02-14 16:33:57 +0800

    update

---
+++ export.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+model_path = "./qwen_sft_model"
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+# 执行动态量化
+quantized_model = torch.quantization.quantize_dynamic(
+    model,
+    {torch.nn.Linear},  # 量化特定层类型（例如Linear层）
+    dtype=torch.qint8   # 选择量化的位数（8位量化）
+)
+
+
+quantized_model.save_pretrained('./qwen2.5-0.5B-int8')
